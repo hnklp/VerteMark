5b61fe3a9e8c5fa46e803fcbee04faae9818f295
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-03-12 12:53:36 +0100

    File manager summary added

--- VerteMark/ObjectClasses/FileManager.cs
+++ VerteMark/ObjectClasses/FileManager.cs
@@ -5,12 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace VerteMark.ObjectClasses {
+    /// <summary>
+    /// Správa a manipulace se soubory pro projekt.
+    /// 
+    /// Zahrnuje:
+    /// * Načítání dat z DICOM souborů.
+    /// * Konverzi DICOM dat do požadovaných formátů.
+    /// * Ukládání projektu a souvisejících souborů.
+    /// </summary>
     internal class FileManager {
-
+        
+        
         public FileManager() {
 
         }
-        //Create a new Folder and fill it with our structure
+        
         public void SaveProject() {
 
         }
