ec83040bdcefea727b88cd6c94b5fe0dc2aca6bc
Hynek Půta <hynek.puta@seznam.cz>
2024-03-21 08:24:07 +0100

    Minor fixes and improvements

--- VerteMark/MainWindows/WelcomeWindow.xaml.cs
+++ VerteMark/MainWindows/WelcomeWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace VerteMark
         public WelcomeWindow()
         {
             InitializeComponent();
+            IDTextBox.Focus();
             utility = new Utility();
         }
 
